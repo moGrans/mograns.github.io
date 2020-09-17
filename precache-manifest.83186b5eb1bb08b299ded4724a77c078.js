42c84ab3b71d1c457c851182ef2804b59928f924
monoco <mcgradytansy@gmail.com>
2020-09-16 22:42:38 -0400

    Updates

---
+++ precache-manifest.83186b5eb1bb08b299ded4724a77c078.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b5d3e86c41d01b2c3368f1bdd72dba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9982bebf5e8b4d358e47",
+    "url": "/static/css/2.cb174c2e.chunk.css"
+  },
+  {
+    "revision": "91429da863ba31d3a152",
+    "url": "/static/css/main.f6122266.chunk.css"
+  },
+  {
+    "revision": "9982bebf5e8b4d358e47",
+    "url": "/static/js/2.4383f7dc.chunk.js"
+  },
+  {
+    "revision": "91429da863ba31d3a152",
+    "url": "/static/js/main.0996602e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
